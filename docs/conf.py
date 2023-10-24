e4e04369b24c777e71ec4c516ef4bffeb13d4023
Taeyoon Hwang <143357930+eastlansing@users.noreply.github.com>
2023-10-24 18:42:25 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+
 project = 'Robust_CI_Acf'
 author = 'Your Name'
-extensions = ['sphinx.ext.autodoc']
-master_doc = 'index'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+
+# The following settings are used by the `sphinx.ext.autodoc` extension to
+# discover and include the docstrings from your Python code.
+autodoc_default_options = {
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
